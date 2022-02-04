e08d549c8b6ba5419b24d5ff05204f97733462fa
Mayur J <mayur.jaganmohan@gmail.com>
2022-02-04 06:12:30 +0530

    service - Update, Delete one, delete many, getAllMovies | controller - getallmovies

--- 24MongoAPI/controller/controller.go
+++ 24MongoAPI/controller/controller.go
@@ -2,10 +2,14 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com.mayur/mongoapi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
@@ -49,4 +53,79 @@ func insertOneMovie(movie model.Netflix) {
 	}
 
 	fmt.Println("Inserted 1 movie in DB with id: ", inserted.InsertedID)
+}
+
+// update 1 record
+
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Modified count: ", result.ModifiedCount)
+}
+
+// Delete 1 record
+
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Movie got deleted with delete count: ", deleteCount)
+}
+
+// Delete all records
+
+func deleteAllMovie() {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Deleted all movies with count: ", deleteResult.DeletedCount)
+}
+
+// Get all movies
+
+func getAllMovies() []primitive.M {
+	cursor, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var movies []primitive.M
+
+	for cursor.Next(context.Background()) {
+		var movie bson.M
+
+		err := cursor.Decode(&movie)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		movies = append(movies, movie)
+	}
+
+	defer cursor.Close(context.Background())
+
+	return movies
+}
+
+// Actual controllers - File
+
+func GetAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www.form-urlencode")
+
+	allMovies := getAllMovies()
+	json.NewEncoder(w).Encode(allMovies)
 }

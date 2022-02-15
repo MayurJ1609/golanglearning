6d130d74dd2d1161187185985d790242081a4e3b
Mayur J <mayur.jaganmohan@gmail.com>
2022-02-15 06:02:07 +0530

    Wait groups, mutex - 13/02/2022 commit

--- 25GoRoutines/main.go
+++ 25GoRoutines/main.go
@@ -2,18 +2,53 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
+var signals = []string{"test"}
+
+var wg sync.WaitGroup // This should be pointers
+var mut sync.Mutex    // pointer
+
 func main() {
-	go greeter("Hello")
-	greeter("World")
+	// go greeter("Hello")
+	// greeter("World")
+
+	websitelist := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+	}
+
+	for _, web := range websitelist {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
+	fmt.Println(signals)
 }
 
-func greeter(s string) {
+// func greeter(s string) {
 
-	for i := 0; i < 6; i++ {
-		time.Sleep(5 * time.Second)
-		fmt.Println(s)
+// 	for i := 0; i < 6; i++ {
+// 		time.Sleep(5 * time.Second)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("OOPS in endpoint")
+	} else {
+		mut.Lock()
+		signals = append(signals, endpoint)
+		mut.Unlock()
+		fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
 	}
+
 }
